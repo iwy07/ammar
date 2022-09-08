27ce5e640684b77336046e67f8aed376c0fc4074
iwy07 <96841996+iwy07@users.noreply.github.com>
2022-09-09 01:16:03 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,6 +6,59 @@ serBtn = document.querySelector(".serBtn"),
 options = document.querySelector(".options");
 btns = document.querySelector(".btns");
 
+var slides = document.querySelectorAll(".slide");
+var btnn = document.querySelectorAll(".btnn");
+let currentSlide = 1;
+
+var manualNav = function(manual){
+slides.forEach((slide) => {
+slide.classList.remove('active');
+btnn.forEach((btn)=> {
+btn.classList.remove('active');
+});
+});
+
+    slides[manual].classList.add('active');
+    btnn[manual].classList.add('active');
+}
+
+btnn.forEach((btn, i) => {
+   btn.addEventListener ("click", ()=> {
+    manualNav(i);
+    currentSlide = i;
+   });
+});
+
+var repeat = function(activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+    var repeater = ()=> {
+        setTimeout(function() {
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+            
+          })
+
+
+            slides[i].classList.add('active');
+            btnn[i].classList.add('active');
+            i++;
+
+           if (slides.length == i) {
+            i = 0;
+           }
+            
+           if (i >= slides.length) {
+            return;
+           }
+           repeater();
+        }, 10000);
+    } 
+    repeater();
+}
+repeat();
+
+
 mopen.onclick = function x(){
     menu.style.right="0";
     menu.style.top="0";
